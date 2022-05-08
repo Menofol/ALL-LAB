0efc30f166951b34d6bf6053fcc351ae26eef8d8
Menofol <45311128+Menofol@users.noreply.github.com>
2022-05-09 00:24:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Umi
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string Vowels = "aAiIuUeEoO";
+			string str;
+
+			Console.Write("String: ");
+			str = Console.ReadLine();
+
+			string[] words = str.Split(new Char[] { ' ', '\t', ',', '.', '!', '?' });
+			int cntWords = words.Length;
+			int[] counters = new int[cntWords];
+			for (int i = 0; i < cntWords; ++i)
+			{
+				for (int j = 0; j < words[i].Length; ++j)
+					if (Vowels.IndexOf(words[i][j]) != -1)
+						counters[i]++;
+			}
+			int maxVowels = counters[0];
+			for (int i = 1; i < cntWords; ++i)
+				if (counters[i] > maxVowels)
+					maxVowels = counters[i];
+
+			string b = Regex.Replace(str, @"\s+", "");
+			Console.WriteLine("Word(s) with the most vowels: ");
+			for (int i = 0; i < cntWords; ++i)
+				if (counters[i] == maxVowels)
+					Console.WriteLine("\t" + words[i]);
+			Console.WriteLine("Romove all space: " + b);
+			Console.WriteLine("String (after trim) = " + str.Trim());
+			Console.ReadLine();
+		}
+	}
+}
